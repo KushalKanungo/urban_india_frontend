7550d56813132aaa90c7e35478f3c3e6c2f48f5e
Kushal Kanungo <kushal.kanungo@metacube.com>
2023-04-08 12:04:39 +0530

    Grid Layout added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BusinessServiceListPageComponent } from './business-service-list-page/business-service-list-page.component';
+import { LoginPageComponent } from './_pages/login-page/login-page.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
   { path: 'services', component: BusinessServiceListPageComponent },
 ];
 
