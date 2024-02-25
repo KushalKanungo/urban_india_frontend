d21eec0103c73ad924cdfaebed6925890d4c72b5
Kushal PC <kushalkhandelwal27@gmail.com>
2024-02-26 00:53:39 +0530

    Added Google Oauth2 login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Route, Router } from '@angular/router';
 import { AuthService, SignInResponse } from '../_services/auth.service';
 import { environment } from 'src/environment/environment';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +16,14 @@ export class LoginComponent {
   private REDIRECT_URI = 'services';
   private clientId = environment.clientId;
   loading = false;
+
+  private afterLogin = (response: SignInResponse) => {
+    this.authService.saveAccessToken(response.accessToken);
+    console.log(this.authService.isAuthorized());
+    this.loading = false;
+    this.router.navigate([this.REDIRECT_URI]);
+    console.log('SignedInSuccessfully');
+  };
 
   ngOnInit() {
     // @ts-ignore
@@ -30,16 +40,27 @@ export class LoginComponent {
         // @ts-ignore
         document.getElementById('buttonDiv'),
         {
-          theme: 'filled',
-          size: 'standard',
-          width: '20%',
-          shape: 'pill',
-          longtitle: false,
+          'width': 20,
+          'height': 60,
+          'longtitle': false,
+          'theme': 'dark',
         }
       );
       // @ts-ignore
       google.accounts.id.prompt((notification: PromptMomentNotification) => {});
     };
+
+    // @ts-ignore
+    google.accounts.id.renderButton(
+      // @ts-ignore
+      document.getElementById('buttonDiv'),
+      {
+        'width': 20,
+        'height': 60,
+        'longtitle': false,
+        'theme': 'dark',
+      }
+    );
   }
 
   signInForm: FormGroup = new FormGroup({
@@ -48,23 +69,26 @@ export class LoginComponent {
   });
 
   googleButttonHandeler() {
-    // @ts-ignore
-    google.accounts.id.initialize({
-      client_id: this.clientId,
-      callback: this.handleCredentialResponse.bind(this),
-      auto_select: false,
-      cancel_on_tap_outside: true,
-    });
-    // @ts-ignore
-    google.accounts.id.prompt((notification: PromptMomentNotification) => {});
+    let button: HTMLButtonElement = document.querySelector("div[role=button]") as HTMLButtonElement
+    button.click()
+  
   }
 
   handleCredentialResponse(response: any) {
     console.log(response.credential);
-
-    this.authService
-      .googleAuth(response.credential)
-      .subscribe({ next: () => {} });
+    this.authService.googleAuth(response.credential).subscribe({
+      next: this.afterLogin,
+      error: (err: HttpErrorResponse) => {
+        if (err.status === 404) {
+          // Navigate to sigup page
+          this.router.navigateByUrl('/signup', {
+            state: {
+              email: err.error.email,
+            },
+          });
+        }
+      },
+    });
   }
 
   signInButtonHandeler() {
@@ -75,13 +99,7 @@ export class LoginComponent {
     };
     this.loading = true;
     this.authService.login(signInDetails).subscribe({
-      next: (res: SignInResponse) => {
-        this.authService.saveAccessToken(res.accessToken);
-        console.log(this.authService.isAuthorized());
-        this.loading = false;
-        this.router.navigate([this.REDIRECT_URI]);
-        console.log('SignedInSuccessfully');
-      },
+      next: this.afterLogin,
     });
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../_services/auth.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
-  constructor(private authService: AuthService, private router: Router) {}
-
+export class SignupComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {
+    if (this.router.getCurrentNavigation()?.extras.state?.['email'])
+      this.email = this.router.getCurrentNavigation()?.extras.state?.['email'];
+  }
+  email: string = '';
   genders = [{ name: 'Male' }, { name: 'Female' }, { name: 'Other' }];
   uploadedFile!: File | any;
+  signupForm: FormGroup = new FormGroup({
+    firstName: new FormControl('Kushal', [Validators.required]),
+    lastName: new FormControl('Kanungo', [Validators.required]),
+    username: new FormControl('k4shal', [Validators.required]),
+    email: new FormControl(this.email, [Validators.email]),
+    gender: new FormControl('Select', [Validators.required]),
+    phone_number: new FormControl('1234567890', [Validators.maxLength(10)]),
+    password: new FormControl('12345', [Validators.required]),
+  });
+
+  ngOnInit(): void {
+    debugger;
+    if (this.email.length) {
+      this.signupForm.get('email')?.setValue(this.email);
+      this.signupForm.get('email')?.disable();
+    }
+  }
 
   onSelect($event: any) {
     for (let file of $event.files) {
@@ -20,23 +40,12 @@ export class SignupComponent {
     }
   }
 
-  signupForm: FormGroup = new FormGroup({
-    firstName: new FormControl('Kushal', [Validators.required]),
-    lastName: new FormControl('Kanungo', [Validators.required]),
-    username: new FormControl('k4shal', [Validators.required]),
-    email: new FormControl('kk@gmial.com', [Validators.email]),
-    gender: new FormControl('Select', [Validators.required]),
-    phone_number: new FormControl('1234567890', [Validators.maxLength(10)]),
-    password: new FormControl('12345', [Validators.required]),
-  });
-
   signupButtonHandeler() {
     let signupDetails = new FormData();
     let data = this.signupForm.value;
 
     data['gender'] = data['gender'].name;
     signupDetails.set('data', JSON.stringify(data));
-    console.log(signupDetails.get('data'));
 
     signupDetails.set('file', this.uploadedFile);
     this.authService.signup(signupDetails).subscribe({
